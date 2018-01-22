39d34d9c7147d3e4730330c725eda60ef08c0b52
amittawde9 <amittawde9@gmail.com>
2018-01-22 17:45:50 +0530

    updated to run on standalone tomcat container

--- src/main/java/com/devops/springframework/SpringBootDevOpsApplication.java
+++ src/main/java/com/devops/springframework/SpringBootDevOpsApplication.java
@@ -2,9 +2,16 @@ package com.devops.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootDevOpsApplication {
+public class SpringBootDevOpsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootDevOpsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDevOpsApplication.class, args);
